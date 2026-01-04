74615fc99bd5b8328555fa1fba8f99492bc4e612
mayu <148281086+mayusrg@users.noreply.github.com>
2026-01-03 17:02:52 -0800

    Add files via upload

---
+++ bronze/DaisyChains.cpp
@@ -0,0 +1,29 @@
+// USACO 2020 December Contest, Bronze Problem 2. Daisy Chains
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int N; cin >> N;
+    int p[N];
+    int ans = 0;
+    for (int i=0; i<N; i++) {
+        cin >> p[i];
+    }
+    for (int i=0; i<N; i++) {
+        for (int j=i; j<N; j++) {
+            int sum = 0;
+            for (int x=i; x<=j; x++) {
+                sum += p[x];
+            }
+            double avg = (double)sum / (j-i+1);
+            for (int x=i; x<=j; x++) {
+                if (p[x] == avg) {
+                    ans++;
+                    break;
+                }
+            }
+        }
+    }
+    cout << ans;
+}
